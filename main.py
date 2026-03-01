3c1f7802a583d3fca018a012a691a48cd9a99fed
wei <mai@mai0313.com>
2026-03-02 00:06:37 +0800

    Refactor download_media function for improved readability and structure

--- main.py
+++ main.py
@@ -12,23 +12,27 @@ class Message(BaseModel):
     date: datetime
 
 
-group_id = "3310384808"
-today = datetime.now().date().strftime("%Y-%m-%d")
-chat_path = Path(f"./data/{today}_{group_id}.json")
+def download_media(group_id: str):
+    today = datetime.now().date().strftime("%Y-%m-%d")
+    chat_path = Path(f"./data/{today}_{group_id}.json")
 
-if not chat_path.exists():
-    chat_path.parent.mkdir(exist_ok=True, parents=True)
-    os.system(f"tdl chat export --chat {group_id} --all --with-content --output {chat_path.as_posix()}")
+    if not chat_path.exists():
+        chat_path.parent.mkdir(exist_ok=True, parents=True)
+        os.system(f"tdl chat export --chat {group_id} --all --with-content --output {chat_path.as_posix()}")
 
-chat_dict = json.loads(chat_path.read_text())
-chat_messages = chat_dict["messages"]
+    chat_content = chat_path.read_text()
+    chat_dict = json.loads(chat_content)
+    chat_messages = chat_dict["messages"]
 
-all_dates = []
-for message in chat_messages:
-    message_obj = Message(**message)
-    # https://t.me/c/3310384808/2025
-    if message_obj.date not in all_dates:
-        all_dates.append(message_obj.date)
-        converted_date = message_obj.date.strftime("%Y-%m-%d")
-        target_url = f"https://t.me/c/{group_id}/{message_obj.id}"
-        os.system(f"tdl download --url {target_url} --group --skip-same")
+    all_dates = []
+    for message in chat_messages:
+        message_obj = Message(**message)
+        # https://t.me/c/3310384808/2025
+        if message_obj.date not in all_dates:
+            all_dates.append(message_obj.date)
+            target_url = f"https://t.me/c/{group_id}/{message_obj.id}"
+            os.system(f"tdl download --url {target_url} --group --skip-same")
+
+if __name__ == "__main__":
+    group_id = "3310384808"
+    download_media(group_id=group_id)
